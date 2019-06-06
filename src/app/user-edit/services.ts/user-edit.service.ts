74ec8dd928cebd82f42a5f40979dfed7c11a4abc
Kredensinh0 <ksawery.hasnik@gmail.com>
2019-06-06 21:16:41 +0200

    avatar update

--- src/app/user-edit/services.ts/user-edit.service.ts
+++ src/app/user-edit/services.ts/user-edit.service.ts
@@ -1,17 +1,19 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class UserEditService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
   httpOptions = {
     headers: new HttpHeaders({
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("token")
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + localStorage.getItem('token')
     })
   };
 
@@ -35,5 +37,22 @@ export class UserEditService {
       JSON.stringify(newPassword),
       this.httpOptions
     );
+  }
+
+  putAvatar(formData): Observable<object> {
+    const options = {
+      headers: new HttpHeaders({
+        Authorization: 'Client-ID 8e29654c3649aea'
+      }),
+      'mimeType': 'multipart/form-data',
+      'contentType': false,
+    };
+    return this.httpClient.post('https://api.imgur.com/3/image', formData, options).pipe(map((res: any) => {
+      const avatarPath = res.data.link; //Imgur response
+      this.httpClient.put(`${environment.apiUrl}/account/avatar`, { 'avatarPath': avatarPath }, this.httpOptions).subscribe((res) => {
+        return res;
+      });
+      return res;
+    }));
   }
 }
