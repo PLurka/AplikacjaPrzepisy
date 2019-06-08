cb613918ed728d22c838e7a732532e612f2c4d95
PLurka <pawellurka@gmail.com>
2019-06-08 22:05:43 +0200

    drobne poprawki do webpacka

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import 'zone.js/dist/zone';
+import 'zone.js/dist/zone-testing';
+import 'reflect-metadata';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+
+
+
 
 if (environment.production) {
   enableProdMode();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
   context: path.resolve(__dirname, '../AplikacjaPrzepisy/'),
   entry: './src/main.ts',
   module: {
@@ -14,15 +14,44 @@ module.exports = {
               options: { configFileName: 'tsconfig.json' }
             } 
           ]
-        }
+        },
+		{
+		  test: /\.css$/,
+		  use: [ 'style-loader', 'css-loader' ]
+		},
+		{
+			test: /\.html$/,
+			use: [
+				{
+					loader: "html-loader",
+					options: { minimize: true }
+				}
+			]
+		}
       ]
   },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js'
   },
-  resolve: {
-	extensions: ['.ts', '.tsx', '.js', '.jsx']
+  optimization:{
+    minimize: false, // <---- disables uglify.
+    // minimizer: [new UglifyJsPlugin()] if you want to customize it.
   },
-  watch: true
+  resolve: {
+	extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+        devServer: {
+            contentBase: path.join(__dirname, "dist"),
+            compress: true,
+            port: 8080,
+            clientLogLevel: "none",
+            historyApiFallback: true
+        }
 };
